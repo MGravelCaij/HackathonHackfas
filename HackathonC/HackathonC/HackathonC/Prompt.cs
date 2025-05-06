7595b28721987b104bb4fae05162e0a17b287260
Mathieu Gravel <mgravel@caij.qc.ca>
2025-05-06 15:16:45 -0400

    Version actuelle du prompt

--- HackathonC/HackathonC/HackathonC/Prompt.cs
+++ HackathonC/HackathonC/HackathonC/Prompt.cs
@@ -12,18 +12,18 @@ namespace HackathonC
         public List<UserChatMessage> messages;
         public Prompt()
         {
-          
+
         }
 
         public List<ChatMessage> CreatePrompt(string legalText)
         {
             List<String> exemples = ["Le tribunal ne peut considérer qu'il est une toute jeune personne."];
             List<string> contreExemples = ["Les actes de l'accusé ont causé un méfait à la victime."];
-            List<string> termesPositifs = ["semble","bien entendu", "évidamment", "de toute évidence", "flagrant"];
+            List<string> termesPositifs = ["semble", "bien entendu", "évidamment", "de toute évidence", "flagrant"];
 
             List<ChatMessage> msgs = [
             new UserChatMessage("Tu es un assistant juridique. Extrait les 5 phrases avec un haut degré de sujectivité dans leur écriture.\n"),
-            new UserChatMessage(" La subjectivité d'un texte signifie le niveau d'assurance que les faits dictés sont justes dans le texte.\n."),
+            new UserChatMessage(" La subjectivité d'un texte signifie que les propos de l'auteur reflètent son opinion personelle au détriment de reposer sur des faits démontrés dans la preuve.\n."),
                             new UserChatMessage($"Voici un exemple d'une phrase subjective : {string.Join("\n",exemples)}."),
                             new UserChatMessage($"Voici un exemple d'une phrase objective :  {string.Join("\n",contreExemples)}."),
                  new UserChatMessage($"Voici une liste de termes que l'on considérons à caractère subjectif :  [{string.Join(",",contreExemples)}].\n"),
